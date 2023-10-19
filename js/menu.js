43aa0777878ccd310ffbf6576c5159fd41da6bfd
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-19 16:32:40 -0300

    menu

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+// Selecionando o elemento que acionará o menu através dae descêndencia
+
+const botaoMenu = document.querySelector("nav h2");
+
+
+// Selecionando a lista/menu atraves da class
+const menu = document.querySelector(".menu");
+
+
+// Selecionando o link que está dentro do nav h2
+const textoBotao = botaoMenu.querySelector("a")
