4546733a25475c61d2c5fa99f033c9991e6f2132
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-24 16:39:31 -0300

    API via CEP

--- js/contato.js
+++ js/contato.js
@@ -4,15 +4,72 @@
 
 const formulario = document.querySelector("form");
 const campoCep = formulario.querySelector("#cep");
-const campoEnderço = formulario.querySelector("#endereco");
+const campoEnderco = formulario.querySelector("#endereco");
 const campoBairro = formulario.querySelector("#bairro");
 const campoCidade = formulario.querySelector("#cidade");
 const campoEstado = formulario.querySelector("#estado");
 const botaoBuscar = formulario.querySelector("#buscar");
+const mensagem = formulario.querySelector("#status");
 
 
 // Detectando o evento de click no botão buscar
-botaoBuscar.addEventListener("click", function () {
-    console.log("Botão acionado...");
+botaoBuscar.addEventListener("click", async function (event) {
+    event.preventDefault();
     
-})
+
+    let cep;  //undefined
+
+    /* Verificando se o CEP Não tem 8 digitos.
+    O operador !== significa "diferente de".*/
+
+    if (campoCep.value.length !== 8){
+        // Alerte o ussuraior sobre o erro de digitação
+        mensagem.textContent = "CEP Inválido!";
+        mensagem.style.color = "red";
+        mensagem.style.backgroundColor = "yellow";
+        
+        // Pare a execução
+        return;
+
+    } else {
+        // Caso contrário(ou seja, tem 8 digitos), guarde o valor
+        cep = campoCep.value;
+    
+    }
+
+    /* AJAX -> Técnica de comunicação assincrona para acessar uma API (www.viacep.com.br) */
+
+    // Etapa 1: preparar a URL da API com o CEP digitado
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    // Etapa 2: acessar a API (com URL) e aguardadr o retorno dela
+
+   const resposta = await fetch(url); 
+
+    // Etapa 3: extrair os dados da respota em formato JSON
+
+    const dados = await resposta.json();
+
+    // Etapa 4: lidar com os dados de reposta (em caso de erro ou sucesso)
+
+    if ("erro" in dados) {
+        mensagem.textContent = "CEP Inexistente!"
+        mensagem.style.color = "white";
+        mensagem.style.backgroundColor = "red";
+        
+    } else {
+        mensagem.textContent = "CEP Encotrado"
+        mensagem.style.color = "white";
+        mensagem.style.backgroundColor = "green";  
+
+        
+        campoEnderco.value = dados.logradouro;
+        campoBairro.value = dados.bairro;
+        campoCidade.value = dados.localidade;
+        campoEstado.value = dados.uf;
+
+    }
+
+
+
+});
