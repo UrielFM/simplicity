0946b0f7d07567e7dff3d7a7fab4d2b96879e1b0
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-25 14:56:31 -0300

    contato

--- js/contato.js
+++ js/contato.js
@@ -87,3 +87,35 @@ botaoBuscar.addEventListener("click", async function (event) {
 
 
 });
+
+
+// Programação do Formspree
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: formulario.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Olá, seja bem-vindo ao Simplicity! Responderemos sua mensagem o mais breve possivel. Obrigado por aguardar!";
+      formulario.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! Algo deu errado! Tente novamente."
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! Algo deu errado! Tente novamente"
+  });
+}
+formulario.addEventListener("submit", handleSubmit)
